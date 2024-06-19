db05a1a8feef19191d73541ca122c2fa995544a3
peifeng_qiu <pengfei_qiu@baofu.com>
2024-06-19 17:48:28 +0800

    最长回文自己的版本

--- leetcode/week02.go
+++ leetcode/week02.go
@@ -18,3 +18,46 @@ func FindMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	}
 	return 0
 }
+
+// LongestPalindrome 5. 最长回文子串
+// 中等
+// 给你一个字符串 s，找到 s 中最长的回文子串。
+//
+// 示例 1：
+// 输入：s = "babad"
+// 输出："bab"
+// 解释："aba" 同样是符合题意的答案。
+// 示例 2：
+// 输入：s = "cbbd"
+// 输出："bb"
+// 提示：
+// 1 <= s.length <= 1000
+// s 仅由数字和英文字母组成
+func LongestPalindrome(s string) string {
+	//12345678987654321
+	//计算总任务数， 字符长度，由于只要查询出一个就行，所以这边就字符长度减1
+	taskNums := len(s)
+	//存储最大回文串
+	longestPalindrome := ""
+	//判断是否是回文
+	isPalindrome := func(tempS string) bool {
+		for h := 0; h < len(tempS)/2; h++ {
+			if tempS[h] != tempS[len(tempS)-1-h] {
+				return false
+			}
+		}
+		return true
+	}
+	for i := 0; i < taskNums; i++ {
+		//判断当前是否满足回文串
+		for j := i + 1; j <= taskNums; j++ {
+			tempS := s[i:j]
+			if isPalindrome(tempS) {
+				if len(tempS) > len(longestPalindrome) {
+					longestPalindrome = tempS
+				}
+			}
+		}
+	}
+	return longestPalindrome
+}
--- leetcode/week02_test.go
+++ leetcode/week02_test.go
@@ -1,7 +1,43 @@
 package leetcode
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFindMedianSortedArrays(t *testing.T) {
 
+}
+
+func TestLongestPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "test1",
+			args: args{
+				s: "babad",
+			},
+			want: "bab",
+		},
+		{
+			name: "test2",
+			args: args{
+				s: "babaab",
+			},
+			want: "baab",
+		},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := LongestPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("LongestPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
